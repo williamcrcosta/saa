a30408148f5f8214c25f095bf9d711c07b0b8213
williamcrcosta <70300983+williamcrcosta@users.noreply.github.com>
2024-06-02 04:06:49 -0300

    Update InstallRemoteAccess.ps1
    Add Telnet-Client install

--- InstallRemoteAccess.ps1
+++ InstallRemoteAccess.ps1
@@ -5,6 +5,7 @@ Install-WindowsFeature Routing -IncludeManagementTools;
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False;
 #Remove-Item -Path "C:\inetpub\wwwroot\iisstart.htm"
 #Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value "TFTEC Cloud - Azure Architecture Week - SAA"
+Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
 
 
 
